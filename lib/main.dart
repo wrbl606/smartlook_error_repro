5c6ffb574cb137500b2523262b1f1df229ec5b31
Marcin Wróblewski <marcin.wroblewski@monterail.com>
2022-06-07 07:43:05 +0200

    Add helper widget above MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return SmartlookHelperWidget(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
